8d4686a871aacc4369d3f3dc5609ff926006e853
FreshGeek001 <memory2026@126.com>
2017-01-20 11:51:15 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :products
+
   namespace :admin do
     resources :products
   end
-  root "welcome#index"
+  root "products#index"
 end
