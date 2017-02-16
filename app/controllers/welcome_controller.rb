5385725139b22b56552d94f19bf71ea68acf0b98
FreshGeek001 <memory2026@126.com>
2017-02-16 11:07:15 +0800

    add carousel in the front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    flash[:notice] = "Good morning"
+    @products = Product.all
+    @products = Product.search(params[:search])
   end
 end
