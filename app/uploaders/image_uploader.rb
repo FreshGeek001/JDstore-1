1a9351d8ca90ff8672e14bd72a5102da4124ce78
FreshGeek001 <memory2026@126.com>
2017-02-22 21:07:47 +0800

    alan-qiniu test

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  # storage :qiniu
-  storage :fog
+  storage :qiniu
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
