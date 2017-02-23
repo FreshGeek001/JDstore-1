fc472edc1c87efe4a9366e7a6fc5024fc46f544e
FreshGeek001 <memory2026@126.com>
2017-02-23 18:47:44 +0800

    figaro

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,14 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :qiniu
+  storage :file
+
   # storage :fog
-  if Rails.env.production?
-    storage :qiniu
-  elsif Rails.env.development?
-    storage :file
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
