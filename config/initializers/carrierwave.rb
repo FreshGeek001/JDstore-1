4107d1869ff6a7908c8e783914c3fc6ad9b8711c
FreshGeek001 <memory2026@126.com>
2017-02-23 21:00:50 +0800

    adjust figo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,5 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
-    config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
